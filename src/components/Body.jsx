dec8df835aead0ded18ffdf69d0d9637c4fdee96
Sumeet Sumeet <sumeet.bx1@gmail.com>
2025-02-10 21:51:57 -0500

    header is working and error handling implemented

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,16 +5,10 @@ const Body = () => {
     return (
         <div className="h-[1000px] grid grid-cols-3 gap-5 p-6 ">
             <Section name="To do">
-                <TaskCard />
-                <TaskCard />
             </Section>
             <Section name="Doing">
-                <TaskCard />
-                <TaskCard />
             </Section>
             <Section name="Done">
-            <TaskCard/>
-            <TaskCard/>
             </Section>
         </div>
     );
